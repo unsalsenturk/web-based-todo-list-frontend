7933a226a8d5040718fec078343593b3b71b6ed3
unsalsenturkk <unsalsenturkk@gmail.com>
2022-02-24 17:38:29 +0300

    added <should todo-list component exists> unit test for Home page

--- src/views/__tests__/Home.spec.js
+++ src/views/__tests__/Home.spec.js
@@ -9,5 +9,10 @@ describe("Home.vue", () => {
             const wrapper = shallowMount(Home)
             expect(wrapper.exists()).toBeTruthy()
         });
+        it('should todo-list component exists', () => {
+            const wrapper = shallowMount(Home)
+            const todo-list = wrapper.findComponent(todo-list)
+            expect(todo-list.exists()).toBeTruthy()
+        });
     })
 })

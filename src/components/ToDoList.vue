48bafb5d64905a666059b7fef9bd356c797554e5
unsalsenturkk <unsalsenturkk@gmail.com>
2022-02-26 02:13:48 +0300

    Added created for TodoList component

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,39 +1,47 @@
 <template>
   <div id="ToDoListMain">
-      <input
-          id="addTxt"
-          type="text"
-      >
-      <input
-          id="addBtn"
-          type="submit"
-          value="Add"
-          @click="addBtnClick"
-          v-model="todo"
-      >
-      <section
-          id="todolist"
-      >
-      </section>
+    <input
+        id="addTxt"
+        type="text"
+    >
+    <input
+        id="addBtn"
+        type="submit"
+        value="Add"
+        @click="addBtnClick"
+        v-model="todo"
+    >
+    <section
+        id="todolist"
+    >
+      <ul v-for="todo of todoList"
+          :key="todo.id">
+        {{todo.description}}
+      </ul>
+    </section>
   </div>
 </template>
 
 <script>
 import api from "@/api";
+
 export default {
   name: "ToDoList",
   data() {
     return {
       todo: '',
-      todoList : []
+      todoList: []
     }
   },
   methods: {
-    async  addBtnClick() {
+    async addBtnClick() {
       const res = await api.addTodo(this.todo)
       this.todoList.push(res)
     }
   },
+  async created() {
+    this.todoList = await api.getTodoList()
+  }
 }
 </script>
 
@@ -50,7 +58,7 @@ export default {
   padding-top: 20px;
 }
 
-#addTxt,#addBtn {
+#addTxt, #addBtn {
   width: 80%;
 }
 

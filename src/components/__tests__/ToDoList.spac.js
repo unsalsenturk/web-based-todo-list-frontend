8304669699e5712840625cf4cf41010a2d585819
unsalsenturkk <unsalsenturkk@gmail.com>
2022-02-26 00:14:24 +0300

    Updated test for ToDoList component

--- src/components/__tests__/ToDoList.spac.js
+++ src/components/__tests__/ToDoList.spac.js
@@ -1,6 +1,7 @@
 import {shallowMount} from "@vue/test-utils";
 import ToDoList from "@/components/ToDoList";
 import API from "@/api";
+import flushPromises from "flush-promises";
 
 
 const mockResponse = [
@@ -10,33 +11,31 @@ const mockResponse = [
 jest.mock("@/api")
 
 describe("ToDoList.vue", () => {
-    beforeAll(() => {
+    let wrapper
+    beforeAll(async () => {
         API.getTodoList.mockResolvedValue(mockResponse)
+        wrapper = shallowMount(ToDoList)
+        await flushPromises()
     })
     describe("should check exist", () => {
         it('should ToDoList component exist', () => {
-            const wrapper = shallowMount(ToDoList)
             expect(wrapper.exists()).toBeTruthy()
         });
         it('should textbox exist', () => {
-            const wrapper = shallowMount(ToDoList)
             const txt = wrapper.find("#addTxt");
             expect(txt.exists()).toBeTruthy()
         });
         it('should add button exist', () => {
-            const wrapper = shallowMount(ToDoList)
             const btn = wrapper.find("#addBtn");
             expect(btn.exists()).toBeTruthy()
         });
         it('should todo list exist', () => {
-            const wrapper = shallowMount(ToDoList)
             const todolist = wrapper.find("#todolist");
             expect(todolist.exists()).toBeTruthy()
         });
     })
     describe("ToDoList component event test", () => {
         it("click event test", async () => {
-            const wrapper = shallowMount(ToDoList)
             wrapper.setMethods({
                 addBtnClick: jest.fn()
             })

d8b7553d490b65fcca747978ba624b40d2fba78f
unsalsenturkk <unsalsenturkk@gmail.com>
2022-02-26 00:05:52 +0300

    Added click event test for ToDoList component

--- src/components/__tests__/ToDoList.spac.js
+++ src/components/__tests__/ToDoList.spac.js
@@ -1,7 +1,18 @@
 import {shallowMount} from "@vue/test-utils";
 import ToDoList from "@/components/ToDoList";
+import API from "@/api";
+
+
+const mockResponse = [
+    {"id": 1, "description": ""}
+]
+
+jest.mock("@/api")
 
 describe("ToDoList.vue", () => {
+    beforeAll(() => {
+        API.getTodoList.mockResolvedValue(mockResponse)
+    })
     describe("should check exist", () => {
         it('should ToDoList component exist', () => {
             const wrapper = shallowMount(ToDoList)
@@ -21,6 +32,16 @@ describe("ToDoList.vue", () => {
             const wrapper = shallowMount(ToDoList)
             const todolist = wrapper.find("#todolist");
             expect(todolist.exists()).toBeTruthy()
+        });
+    })
+    describe("ToDoList component event test", () => {
+        it("click event test", async () => {
+            const wrapper = shallowMount(ToDoList)
+            wrapper.setMethods({
+                addBtnClick: jest.fn()
+            })
+            await wrapper.find("#addBtn").trigger("click")
+            expect(wrapper.vm.addBtnClick).toHaveBeenCalled()
         });
     })
 })

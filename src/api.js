5c41b4fb425f3148114b222a54d7e6006f8b49b3
unsalsenturkk <unsalsenturkk@gmail.com>
2022-02-25 16:16:59 +0300

    Added api.js

---
+++ src/api.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import adapter from "axios/lib/adapters/http"
+
+axios.defaults.adapter(adapter)
+
+export class API{
+    constructor(url) {
+        if (url === undefined || url === "") {
+            url = process.env.VUE_APP_BASE_API_URL;
+        }
+        if (url.endsWith("/")) {
+            url = url.substr(0, url.length - 1)
+        }
+        this.url = url
+    }
+
+    withPath(path) {
+        if (!path.startsWith("/")) {
+            path = "/" + path
+        }
+        return `${this.url}${path}`
+    }
+
+    async getTodoList() {
+        return axios
+            .get(this.withPath('/getTodoList'))
+            .then(r => r.data)
+    }
+
+    async addTodo(todo) {
+        return axios
+            .post(this.withPath('/addTodo'), {
+                task_description: todo
+            })
+            .then(r => r.data)
+    }
+}
+
+export default new API("http://localhost:3000/api/v1")
